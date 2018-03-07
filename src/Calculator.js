88626d7dd2fd5a0d8baabc58be60ab592d5cabd6
brantchyoga <brantrporter@gmail.com>
2018-03-06 19:02:38 -0800

    Added -,/,* operations

--- src/Calculator.js
+++ src/Calculator.js
@@ -5,20 +5,38 @@ class Calculator extends Component {
     super()
     this.state = {
       value1: 0,
-      value2: 0
+      value2: 0,
+      sum: ''
     }
-    this.handleChange = this.handleChange.bind(this)
   }
-  handleChange(index,e){
+  handleChange = (index,e) => {
       if(index === 1){
         this.setState({value1: e.target.value})
       } else {
         this.setState({value2: e.target.value})
       }
   }
-
+  adding = () => {
+    this.setState({
+      sum: parseInt(this.state.value1) + parseInt(this.state.value2)
+    })
+  }
+  substracting = () => {
+    this.setState({
+      sum: parseInt(this.state.value1) - parseInt(this.state.value2)
+    })
+  }
+  dividing = () => {
+    this.setState({
+      sum: parseInt(this.state.value1) / parseInt(this.state.value2)
+    })
+  }
+  multiply = () => {
+    this.setState({
+      sum: parseInt(this.state.value1) * parseInt(this.state.value2)
+    })
+  }
   render(){
-    let sum = parseInt(this.state.value1) + parseInt(this.state.value2)
 
     return(
       <div className="container">
@@ -26,10 +44,12 @@ class Calculator extends Component {
 
         <div className="add">
           <input type="number" onChange={(e)=>this.handleChange(1,e)}/>
-          <span>+</span>
           <input type="number" onChange={(e)=>this.handleChange(2,e)}/>
-          <span>=</span>
-          <h3>{sum}</h3>
+          <button className="inline blue" onClick={this.adding}>+</button>
+          <button className="inline blue" onClick={this.substracting}>-</button>
+          <button className="inline blue" onClick={this.dividing}>/</button>
+          <button className="inline blue" onClick={this.multiply}>*</button>
+          <h3>{this.state.sum}</h3>
         </div>
       </div>
     )
